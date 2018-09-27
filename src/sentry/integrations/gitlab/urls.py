8c5d27b132db5ba183f1d232fab33c0b104aa6c2
Lauryn Brown <lauryndbrown@gmail.com>
2018-09-27 16:57:13 -0700

    Adding client information needed to create webhooks.

--- src/sentry/integrations/gitlab/urls.py
+++ src/sentry/integrations/gitlab/urls.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, print_function
 
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url
 
+from .webhooks import GitlabWebhookEndpoint
 
 urlpatterns = patterns(
     '',
+    url(r'^webhooks/$', GitlabWebhookEndpoint.as_view()),
 )
