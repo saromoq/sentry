d526285344092453e63e9d43aca721d9b3bce014
Lauryn Brown <lauryndbrown@gmail.com>
2018-09-28 10:18:32 -0700

    fixed issues Matte brought up.

--- src/sentry/tasks/integrations.py
+++ src/sentry/tasks/integrations.py
@@ -218,14 +218,12 @@ def migrate_repo(repo_id, integration_id, organization_id):
             integration,
             Organization.objects.get(id=organization_id),
         ).call()
-# Particulariliry For VSTS
-# TODO(lb): I want to say do this every 6 hours
 
 
 @instrumented_task(
     name='sentry.tasks.integrations.kickoff_vsts_subscription_check',
     queue='integrations',
-    default_retry_delay=60 * 5,  # TODO(lb): not sure what this should be
+    default_retry_delay=60 * 5,
     max_retries=5,
 )
 @retry()
@@ -249,7 +247,7 @@ def kickoff_vsts_subscription_check():
 
         vsts_subscription_check.apply_async(
             kwargs={
-                'integration': integration,
+                'integration_id': integration.id,
                 'organization_id': organization_id,
             }
         )
@@ -258,11 +256,12 @@ def kickoff_vsts_subscription_check():
 @instrumented_task(
     name='sentry.tasks.integrations.vsts_subscription_check',
     queue='integrations',
-    default_retry_delay=60 * 5,  # TODO(lb): not sure what this should be
+    default_retry_delay=60 * 5,
     max_retries=5,
 )
-@retry(exclude=(ApiError, ApiUnauthorized))
-def vsts_subscription_check(integration, organization_id, **kwargs):
+@retry(exclude=(ApiError, ApiUnauthorized, Integration.DoesNotExist))
+def vsts_subscription_check(integration_id, organization_id, **kwargs):
+    integration = Integration.objects.get(id=integration_id)
     installation = integration.get_installation(organization_id=organization_id)
     client = installation.get_client()
     subscription_id = integration.metadata['subscription']['id']
